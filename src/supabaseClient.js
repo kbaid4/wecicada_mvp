6cf9c1257c872ee2ac0d43a705308cd0ff4ca5e4
Kushagra Baid <kushagrabaid@Kushagras-MacBook-Pro.local>
2025-06-04 23:29:16 +0200

    Use environment variables for Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oesorptwsvoydqhmkbof.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lc29ycHR3c3ZveWRxaG1rYm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MTg1MjYsImV4cCI6MjA2NDA5NDUyNn0.pQXEAnFIUqs-iTE2awWcadT1UZY6G_v3Wl7I65x4cUw';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
